4908a1adb3151cbc3aefe7a90ab0bed2d7d3625b
Sean Zheng <blackhorseya@gmail.com>
2025-02-27 20:49:52 +0800

    feat: Add slog-based logger and update logging tests (#2)
    ### **User description**
    This pull request introduces a new logging mechanism using `slog` in
    addition to the existing `zap` logger. The changes include importing the
    `slog` package, adding a new function to initialize the `slog` logger,
    and updating the tests to cover the new logger.
    
    ### New Logging Mechanism:
    
    *
    [`logger/logger.go`](diffhunk://#diff-606d907279d517af7dab4f35f571671388c95c5a05e2c9fe033b08e0b4588348R4):
    Imported the `slog` package and added the `NewSlogLogger` function to
    initialize the `slog` logger with different formats and log levels.
    [[1]](diffhunk://#diff-606d907279d517af7dab4f35f571671388c95c5a05e2c9fe033b08e0b4588348R4)
    [[2]](diffhunk://#diff-606d907279d517af7dab4f35f571671388c95c5a05e2c9fe033b08e0b4588348R47-R73)
    
    ### Test Updates:
    
    *
    [`logger/logger_test.go`](diffhunk://#diff-b9d87dbc6149275d8199d707bb7236e54a99d46082c277b047a4a0d81be80ddcL10-R26):
    Added a new test function `TestSlogLogger` to verify the initialization
    and functionality of the `slog` logger. Also updated the `TestZapLogger`
    function to handle errors and test the logger output.
    
    
    ___
    
    ### **PR Type**
    - Enhancement
    - Tests
    
    
    
    ___
    
    ### **Description**
    - Introduce slog logger initialization function.
    
    - Add configurable JSON/text formatter.
    
    - Refactor log level handling with unmarshal.
    
    - Enhance tests for new slog logger.
    
    
    ___
    
    
    
    ### **Changes walkthrough** 📝
    <table><thead><tr><th></th><th align="left">Relevant
    files</th></tr></thead><tbody><tr><td><strong>Enhancement</strong></td><td><table>
    <tr>
      <td>
        <details>
    <summary><strong>logger.go</strong><dd><code>Add new slog logger
    initialization.</code>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
    &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
    &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; </dd></summary>
    <hr>
    
    logger/logger.go
    
    <li>Imported slog package.<br> <li> Added NewSlogLogger function.<br>
    <li> Configurable handler for JSON or text.<br> <li> Sets default logger
    with proper log level.
    
    
    </details>
    
    
      </td>
    <td><a
    href="https://github.com/blackhorseya/go-libs/pull/2/files#diff-606d907279d517af7dab4f35f571671388c95c5a05e2c9fe033b08e0b4588348">+28/-0</a>&nbsp;
    &nbsp; </td>
    
    </tr>
    </table></td></tr><tr><td><strong>Tests</strong></td><td><table>
    <tr>
      <td>
        <details>
    <summary><strong>logger_test.go</strong><dd><code>Update logger tests
    for slog.</code>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
    &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
    &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
    </dd></summary>
    <hr>
    
    logger/logger_test.go
    
    <li>Changed TestZapLogger to JSON format.<br> <li> Added TestSlogLogger
    function.<br> <li> Included error handling test for slog.
    
    
    </details>
    
    
      </td>
    <td><a
    href="https://github.com/blackhorseya/go-libs/pull/2/files#diff-b9d87dbc6149275d8199d707bb7236e54a99d46082c277b047a4a0d81be80ddc">+17/-1</a>&nbsp;
    &nbsp; </td>
    
    </tr>
    </table></td></tr></tr></tbody></table>
    
    ___
    
    > <details> <summary> Need help?</summary><li>Type <code>/help how to
    ...</code> in the comments thread for any questions about PR-Agent
    usage.</li><li>Check out the <a
    href="https://qodo-merge-docs.qodo.ai/usage-guide/">documentation</a>
    for more information.</li></details>
    
    ---------
    
    Signed-off-by: blackhorseya <blackhorseya@gmail.com>

--- logger/logger.go
+++ logger/logger.go
@@ -1,6 +1,7 @@
 package logger
 
 import (
+	"log/slog"
 	"os"
 
 	"go.uber.org/zap"
@@ -40,6 +41,33 @@ func NewZapLogger(options Options) (*zap.Logger, error) {
 	logger := zap.New(core, zap.AddCaller(), zap.AddStacktrace(zap.ErrorLevel))
 
 	zap.ReplaceGlobals(logger)
+
+	return logger, nil
+}
+
+// NewSlogLogger initializes the logging instance.
+func NewSlogLogger(options Options) (*slog.Logger, error) {
+	var level slog.Level
+	err := level.UnmarshalText([]byte(options.Level))
+	if err != nil {
+		return nil, err
+	}
+
+	opts := &slog.HandlerOptions{
+		AddSource: true,
+		Level:     level,
+	}
+
+	var handler slog.Handler
+	switch options.Format {
+	case "json":
+		handler = slog.NewJSONHandler(os.Stdout, opts)
+	default:
+		handler = slog.NewTextHandler(os.Stdout, opts)
+	}
+
+	logger := slog.New(handler)
+	slog.SetDefault(logger)
 
 	return logger, nil
 }
