396b96ede1c72f5e02d0a59eee4f7000b5e57dd5
Sean Zheng <blackhorseya@gmail.com>
2025-02-27 22:28:30 +0800

    refactor: standardize logging with new Logger interface implementations (#3)
    ### **PR Type**
    - Enhancement
    
    
    
    ___
    
    ### **Description**
    - Remove legacy logger implementations from logger.go
    
    - Introduce Logger interface in logger.go file
    
    - Migrate NewSlogLogger and NewZapLogger to separate files
    
    
    ___
    
    
    
    ### **Changes walkthrough** 📝
    <table><thead><tr><th></th><th align="left">Relevant
    files</th></tr></thead><tbody><tr><td><strong>Enhancement</strong></td><td><table>
    <tr>
      <td>
        <details>
    <summary><strong>logger.go</strong><dd><code>Define Logger interface and
    remove old implementations</code>&nbsp; &nbsp; &nbsp; </dd></summary>
    <hr>
    
    logger/logger.go
    
    <li>Removed NewZapLogger and NewSlogLogger implementations<br> <li>
    Added Logger interface definition<br> <li> Cleaned up unused imports for
    zap and slog
    
    
    </details>
    
    
      </td>
    <td><a
    href="https://github.com/blackhorseya/go-libs/pull/3/files#diff-606d907279d517af7dab4f35f571671388c95c5a05e2c9fe033b08e0b4588348">+6/-61</a>&nbsp;
    &nbsp; </td>
    
    </tr>
    
    <tr>
      <td>
        <details>
    <summary><strong>slog.go</strong><dd><code>Add NewSlogLogger
    implementation in separate file</code>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
    &nbsp; &nbsp; &nbsp; </dd></summary>
    <hr>
    
    logger/slog.go
    
    <li>Added NewSlogLogger implementation<br> <li> Migrated slog logger
    functionality from logger.go<br> <li> Configured handler options for
    slog logger
    
    
    </details>
    
    
      </td>
    <td><a
    href="https://github.com/blackhorseya/go-libs/pull/3/files#diff-eaacd7ed155c829fa71e358d09752203e209894410e320e6a718ff66e21ee6dc">+33/-0</a>&nbsp;
    &nbsp; </td>
    
    </tr>
    
    <tr>
      <td>
        <details>
    <summary><strong>zap.go</strong><dd><code>Add NewZapLogger
    implementation in separate file</code>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
    &nbsp; &nbsp; &nbsp; &nbsp; </dd></summary>
    <hr>
    
    logger/zap.go
    
    <li>Added NewZapLogger implementation in new file<br> <li> Recreated zap
    logger functionality from logger.go<br> <li> Configured zap logging
    outputs and encoder options
    
    
    </details>
    
    
      </td>
    <td><a
    href="https://github.com/blackhorseya/go-libs/pull/3/files#diff-6deae60ed524f5033424ecfa6fadc303c35051abb7a28f048cc9c7e4363adb79">+36/-0</a>&nbsp;
    &nbsp; </td>
    
    </tr>
    </table></td></tr></tr></tbody></table>
    
    ___
    
    > <details> <summary> Need help?</summary><li>Type <code>/help how to
    ...</code> in the comments thread for any questions about PR-Agent
    usage.</li><li>Check out the <a
    href="https://qodo-merge-docs.qodo.ai/usage-guide/">documentation</a>
    for more information.</li></details>
    
    Signed-off-by: blackhorseya <blackhorseya@gmail.com>

--- logger/logger.go
+++ logger/logger.go
@@ -1,13 +1,5 @@
 package logger
 
-import (
-	"log/slog"
-	"os"
-
-	"go.uber.org/zap"
-	"go.uber.org/zap/zapcore"
-)
-
 // Options is the logging options.
 type Options struct {
 	// Level is the log level. options: debug, info, warn, error, dpanic, panic, fatal (default: info)
@@ -17,57 +9,10 @@ type Options struct {
 	Format string `json:"format" yaml:"format" mapstructure:"format"`
 }
 
-// NewZapLogger initializes the logging instance.
-func NewZapLogger(options Options) (*zap.Logger, error) {
-	level := zap.NewAtomicLevel()
-	err := level.UnmarshalText([]byte(options.Level))
-	if err != nil {
-		return nil, err
-	}
-
-	cw := zapcore.Lock(os.Stdout)
-	zapConfig := zap.NewDevelopmentEncoderConfig()
-	zapConfig.EncodeLevel = zapcore.CapitalColorLevelEncoder
-	enc := zapcore.NewConsoleEncoder(zapConfig)
-	if options.Format == "json" {
-		zapConfig = zap.NewProductionEncoderConfig()
-		enc = zapcore.NewJSONEncoder(zapConfig)
-	}
-
-	cores := make([]zapcore.Core, 0)
-	cores = append(cores, zapcore.NewCore(enc, cw, level))
-
-	core := zapcore.NewTee(cores...)
-	logger := zap.New(core, zap.AddCaller(), zap.AddStacktrace(zap.ErrorLevel))
-
-	zap.ReplaceGlobals(logger)
-
-	return logger, nil
-}
-
-// NewSlogLogger initializes the logging instance.
-func NewSlogLogger(options Options) (*slog.Logger, error) {
-	var level slog.Level
-	err := level.UnmarshalText([]byte(options.Level))
-	if err != nil {
-		return nil, err
-	}
-
-	opts := &slog.HandlerOptions{
-		AddSource: true,
-		Level:     level,
-	}
-
-	var handler slog.Handler
-	switch options.Format {
-	case "json":
-		handler = slog.NewJSONHandler(os.Stdout, opts)
-	default:
-		handler = slog.NewTextHandler(os.Stdout, opts)
-	}
-
-	logger := slog.New(handler)
-	slog.SetDefault(logger)
-
-	return logger, nil
+// Logger is the logging interface.
+type Logger interface {
+	Debug(msg string, fields ...any)
+	Info(msg string, fields ...any)
+	Warn(msg string, fields ...any)
+	Error(msg string, fields ...any)
 }
